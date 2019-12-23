6a2738893c56aea54e2db67d09ca473a3e3c81e4
MGedney1 <maurice@gedney.eu>
2019-12-23 17:57:24 +0000

    Added worst case for bubble

--- Sorting_Algorithms.py
+++ Sorting_Algorithms.py
@@ -8,6 +8,12 @@ def random_numbers(n):      #Creating the random array
         start_list.append(random.randint(0,100))
     return start_list
 
+def worst_case(n):
+    worst_case = list(range(n))
+    worst_case.reverse()
+    return worst_case
+
+
 def bubble_sort(arr):        #Bubble sort
     start = time.time()
     index = len(arr) - 1
@@ -25,11 +31,20 @@ def bubble_sort(arr):        #Bubble sort
 
 
 n = list(range(10,1000,10))     #Running bubble sort for a range of array lengths and showing a graph of the times taken against array size
+
+
 times_bubble = []
+times_bubble_worst_case = []
+
 for x in range(len(n)):
     times_bubble.append(bubble_sort(random_numbers(n[x])))
-plt.plot(n,times_bubble)
+    times_bubble_worst_case.append(bubble_sort(worst_case(n[x])))
+
+
+plt.plot(n,times_bubble,label='Random Run')
+plt.plot(n,times_bubble_worst_case,label='Worst Case')
 plt.xlabel('Array Length')
 plt.ylabel('Time Taken /s')
 plt.title('Bubble Sort')
+plt.legend()
 plt.show()
